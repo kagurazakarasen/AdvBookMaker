3f2eba217883f1d00bbcc266809a9fee75af5036
Genbu Hase <hasegenbu@gmail.com>
2017-03-16 16:17:06 +0900

    AdvGameBookMakerからAdvBookMakerに変更。

---
+++ Project/AdvBookMaker On C-Sharp/Base.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvBookMaker {
+	public partial class Base : Form {
+		public Base() {
+			InitializeComponent();
+		}
+
+		private void Manager_Editor_AddScene_Click(object Sender, EventArgs Event) {
+			
+		}
+
+		private void MenuBar_File_Save_Click(object Sender, EventArgs Error) {
+			
+		}
+	}
+}
