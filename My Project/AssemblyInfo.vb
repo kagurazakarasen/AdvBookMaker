c45776d9b31f63cacc1098b055b7f87271d37cca
rasen_PC <kagurazakarasen@gmail.com>
2017-03-21 00:09:29 +0900

    Revert "Version 0.0.1.1"
    This reverts commit 50fa43275452e5e495d87d14eb7f5a02072b0142.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.1.1")>
-<Assembly: AssemblyFileVersion("0.0.1.1")>
+<Assembly: AssemblyVersion("0.0.0.1")>
+<Assembly: AssemblyFileVersion("0.0.0.1")>
